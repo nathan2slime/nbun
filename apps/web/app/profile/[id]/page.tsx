e0518acfaf9af85e0d0694caa702dd93a8dac9cf
Jhonathan <nathan3boss@gmail.com>
2024-11-28 00:21:46 -0300

    refactor: lint

--- apps/web/app/profile/[id]/page.tsx
+++ apps/web/app/profile/[id]/page.tsx
@@ -1,33 +1,38 @@
-"use client"
+'use client'
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { PenLine, Plus } from "lucide-react"
-import { AvatarsDialog } from "~/components/avatars-dialog"
-import { Button } from "~/components/ui/button"
-import { ProfileSchema, profileSchema } from "~/lib/schemas/profile.schemas"
-import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { PenLine, Plus } from 'lucide-react'
 import Image from 'next/image'
-import { Card } from "~/components/ui/card"
+import { useForm } from 'react-hook-form'
+import { AvatarsDialog } from '~/components/avatars-dialog'
+import { Button } from '~/components/ui/button'
+import { Card } from '~/components/ui/card'
+import { ProfileSchema, profileSchema } from '~/lib/schemas/profile.schemas'
 
+import { DialogClose } from '@radix-ui/react-dialog'
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
-} from "~/components/ui/dialog"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "~/components/ui/form"
-import { Input } from "~/components/ui/input"
-import { DialogClose } from "@radix-ui/react-dialog"
-
+  DialogTrigger
+} from '~/components/ui/dialog'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '~/components/ui/form'
+import { Input } from '~/components/ui/input'
 
 const Profile = () => {
-
   const user = {
-    name: "Samuel Sanderson",
-    avatar: "woin",
+    name: 'Samuel Sanderson',
+    avatar: 'woin'
   }
 
   const form = useForm<ProfileSchema>({
@@ -44,89 +49,91 @@ const Profile = () => {
   const values = form.watch()
 
   const changeProfile = (values: ProfileSchema) => {
-    console.log(values);
-    
+    console.log(values)
   }
 
   return (
     <div className="min-h-screen w-full p-3">
-      <p className="font-semibold text-2xl tracking-wide text-center">Profile</p>
+      <p className="text-center font-semibold text-2xl tracking-wide">
+        Profile
+      </p>
       <div className="mt-8 flex w-full flex-col items-center gap-2">
-            <div className="flex items-center gap-2">
-                <Image
-                  className="size-[80px] rounded-full object-cover"
-                  src={'/assets/'.concat(values.avatar).concat('.jpg')}
-                  alt={values.avatar}
-                  width={60}
-                  height={60}
-                />
+        <div className="flex items-center gap-2">
+          <Image
+            className="size-[80px] rounded-full object-cover"
+            src={'/assets/'.concat(values.avatar).concat('.jpg')}
+            alt={values.avatar}
+            width={60}
+            height={60}
+          />
 
-              <AvatarsDialog
-                avatar={values.avatar}
-                onChange={avatar => setValue('avatar', avatar)}
-              >
-                <Button size="icon" variant="outline" className="rounded-full">
-                  <Plus />
-                </Button>
-              </AvatarsDialog>
+          <AvatarsDialog
+            avatar={values.avatar}
+            onChange={avatar => setValue('avatar', avatar)}
+          >
+            <Button size="icon" variant="outline" className="rounded-full">
+              <Plus />
+            </Button>
+          </AvatarsDialog>
+        </div>
+
+        <p className="text-xl"> Olá, {values.username}!</p>
+
+        <div className="flex w-full max-w-64 space-y-3">
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="outline" className="w-full text-lg" size="sm">
+                <PenLine className="mr-2 h-4 w-4" />
+                Editar Perfil
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Editar perfil de usuário</DialogTitle>
+              </DialogHeader>
+
+              <Form {...form}>
+                <form onSubmit={form.handleSubmit(changeProfile)}>
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl>
+                          <Input placeholder="fulano" {...field} />
+                        </FormControl>
+                        <FormDescription>seu nome de perfil.</FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <DialogFooter className="mt-1">
+                    <DialogClose asChild>
+                      <Button className="inline-block" type="submit">
+                        Salvar alterações
+                      </Button>
+                    </DialogClose>
+                  </DialogFooter>
+                </form>
+              </Form>
+            </DialogContent>
+          </Dialog>
+        </div>
+
+        <div className="my-8 grid w-full max-w-64 grid-cols-2 gap-4">
+          <Card className="p-4 text-center">
+            <div className="font-bold text-2xl">15</div>
+            <div className="text-muted-foreground text-sm">Quizzes Criados</div>
+          </Card>
+          <Card className="p-4 text-center">
+            <div className="font-bold text-2xl">42</div>
+            <div className="text-muted-foreground text-sm">
+              Quizzes participados
             </div>
-
-            <p className="text-xl"> Olá, {values.username}!</p>
-
-            <div className="w-full space-y-3 max-w-64 flex">
-
-              <Dialog>
-                <DialogTrigger asChild >
-                  <Button variant="outline" className="w-full text-lg" size="sm">
-                    <PenLine className="mr-2 h-4 w-4" />
-                    Editar Perfil
-                  </Button>
-                </DialogTrigger>
-                <DialogContent>
-                  <DialogHeader>
-                    <DialogTitle>Editar perfil de usuário</DialogTitle>
-                  </DialogHeader>
-
-                  <Form {...form}>
-                    <form onSubmit={form.handleSubmit(changeProfile)}>
-                    <FormField
-                      control={form.control}
-                      name="username"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Username</FormLabel>
-                          <FormControl>
-                            <Input placeholder="fulano" {...field} />
-                          </FormControl>
-                          <FormDescription>seu nome de perfil.</FormDescription>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                      <DialogFooter className="mt-1">
-                        <DialogClose asChild>
-                          <Button className="inline-block" type="submit">Salvar alterações</Button>
-                        </DialogClose>
-                      </DialogFooter>
-                    </form>
-                  </Form>
-                  
-                </DialogContent>
-              </Dialog>
-            
-            </div>
-
-            <div className="w-full grid grid-cols-2 gap-4 my-8 max-w-64">
-              <Card className="p-4 text-center">
-                <div className="text-2xl font-bold">15</div>
-                <div className="text-sm text-muted-foreground">Quizzes Criados</div>
-              </Card>
-              <Card className="p-4 text-center">
-                <div className="text-2xl font-bold">42</div>
-                <div className="text-sm text-muted-foreground">Quizzes participados</div>
-              </Card>
-            </div>
-          </div>
+          </Card>
+        </div>
+      </div>
     </div>
   )
 }
